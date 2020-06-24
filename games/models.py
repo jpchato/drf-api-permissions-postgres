fa1cdf71e9f419d80317d64b963b412d87a830dd
jpchato <jpchato@gmail.com>
2020-06-23 21:00:03 -0700

    first commit

---
+++ games/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField() 
+    player = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
